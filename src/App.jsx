0d422f24b6ecb2fba69fe21136f30848b48f7bcb
Логунова Алина Дмитриевна <alina.logunova@norbit.ru>
2025-03-20 23:48:22 +0300

    feat: added structure for main page

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,31 @@
 import "./App.css";
 
 const App = () => {
-  return <div>С днем рождения!!!</div>;
+  const todayDate = new Date();
+  const day = todayDate.getDate();
+  const month = todayDate.getMonth() + 1;
+
+  const text = import.meta.env.VITE_TEXT;
+
+  if (day === 23 && month === 3) {
+    return (
+      <div>
+        <div>С днем рождения!</div>
+        <div>
+          <button>Получить пожелание на день</button>
+          <button>Прочитать поздравление</button>
+        </div>
+      </div>
+    );
+  }
+
+  return (
+    <div>
+      <div>Настеньчик, доброго дня!</div>
+      <div>{text}</div>
+      <button>Получить пожелание на день</button>
+    </div>
+  );
 };
 
 export default App;
